41055f8d3775b1f1c1b8eb078c081b97bad742fb
Rhys Shaw <rhysc3p0@gmail.com>
2023-06-08 12:11:33 +0100

    update

--- C++&Py/add_one.cpp
+++ C++&Py/add_one.cpp
@@ -11,4 +11,12 @@ int multiply(int a, int b) {
     return a * b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
+
 }
---
+++ C++-experimenting/test.cpp
@@ -0,0 +1,12 @@
+// of cout object
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 4;
+    int y = 5;
+    int z = x + y;
+    cout << "The sum of x and y is: ";
+    cout << z;
+    return 0;
+}
